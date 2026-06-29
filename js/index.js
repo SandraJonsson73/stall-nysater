f9d8ea69d5a619fa1aae21cbb9976c87f141bb6e
SandraJonsson73 <stallnysater@gmail.com>
2026-06-29 21:27:53 +0200

    Uppdatera nyheter och resultat för hästarna, inklusive nya tävlingsresultat och justeringar av befintlig information.

--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const container = document.querySelector(".image-and-news");
+document.addEventListener("DOMContentLoaded", function () {
+  const container = document.querySelector(".image-and-news");
 
-    const newsBox = document.createElement("div");
-    newsBox.classList.add("news-boxen");
-    newsBox.innerHTML = `
+  const newsBox = document.createElement("div");
+  newsBox.classList.add("news-boxen");
+  newsBox.innerHTML = `
       <h3>Senaste nytt</h3>
-      <p>25/10-25, Bergsåker<br>
-      Helios storebror Hackels Lumiere VANN i sin 10:e start. 12.000 kr och nytt rekord på 1.17,9. <br>
-      Stort Grattis till hela gänget</p>
+      <p>25/5-26, Mantorp<br>
+      Idag skulle Eve kört Midde i premieloppet, men eftersom Midde råkade bryta hennes finger så fick Oskar Svanberg köra honom. Midde klarade premien galant på tiden 1.31,0g och 20.000 kr kunde ägaren/tränaren inkassera. 
+      </p>
     `;
 
-    container.appendChild(newsBox);
-  });
+  container.appendChild(newsBox);
+});
